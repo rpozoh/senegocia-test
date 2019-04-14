6137cddfd42a4e472a569e07ac3eef63806299cf
Mario <rpozoh@gmail.com>
2019-04-13 21:30:59 -0400

    Commit inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
+// Componentes
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+
+// Rutas
+import { APP_ROUTES } from './app.routes';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot( APP_ROUTES,{ useHash : true } )
   ],
   providers: [],
   bootstrap: [AppComponent]
